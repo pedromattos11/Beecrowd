eca9b1c5bb086215c3c1d506aa1b1396f7996c6f
Pedro Mattos <pedro.hmattos19@gmail.com>
2023-10-17 00:20:54 -0300

    Oct 17th, 23

--- 1066.js
+++ 1066.js
@@ -1,25 +1,27 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
-var lines = input.split('\r\n').map(Number);
+var lines = input.split('\n').map(Number);
 
-var var_Pares=0;
+var var_Pares=-1;
 var var_Impar=0;
 var var_Negativo=0;
 var var_Positivo=0;
-var var_Contadora=0
 
-for(var_Contadora < lines.length; var_Contadora++;){
-    while(lines[var_Contadora] % 2 === 0){ // Contagem de números pares
+for(var var_Contadora=0; var_Contadora < lines.length; var_Contadora++){
+    if(lines[var_Contadora] % 2 === 0){ // Contagem de números pares
         var_Pares++;
     }
-    if(lines[var_Contadora] % 2 === 1){ // Contagem de números impares
+    if(lines[var_Contadora] % 2 !== 0){ // Contagem de números impares
         var_Impar++;
     }
     if(lines[var_Contadora] > 0){ // Contagem de números positivos
         var_Positivo++;
     }
+    if(lines[var_Contadora] < 0){ // Contagem de números positivos
+        var_Negativo++;
+    }
 }
 
-console.log(var_Pares + " valores pares");
-console.log(var_Impar + " valores impares");
-console.log(var_Positivo + " valores positivos");
-console.log(var_Negativo + " valores negativos");
+console.log(var_Pares + " valor(es) par(es)");
+console.log(var_Impar + " valor(es) impar(es)");
+console.log(var_Positivo + " valor(es) positivo(s)");
+console.log(var_Negativo + " valor(es) negativo(s)");
