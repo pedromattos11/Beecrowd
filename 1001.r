946e930cf4815fffa30db2ceb3d5b152af405fa4
Pedro Mattos <pedro.hmattos19@gmail.com>
2023-10-23 00:38:13 -0300

    Update Oct 23rd, 23

---
+++ 1001.r
@@ -0,0 +1,9 @@
+# Leia os dois valores inteiros
+A <- as.integer(readline(prompt = "Digite o valor de A: "))
+B <- as.integer(readline(prompt = "Digite o valor de B: "))
+
+# Calcule a soma
+X <- A + B
+
+# Imprima o resultado formatado
+cat("X = ", X, "\n")
