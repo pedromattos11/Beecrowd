f56d924c2a7d8b4f06bed96c7e782b430dafe2a2
Pedro Mattos <pedro.hmattos19@gmail.com>
2023-10-29 22:53:27 -0300

    Update Oct 29th, 23

--- 2621.sql
+++ 2621.sql
@@ -1,4 +1,4 @@
-SELECT p.name
-FROM products p
-INNER JOIN providers pr ON p.id_providers = pr.id
-WHERE p.amount >= 10 AND p.amount <= 20 AND LEFT(pr.name, 1) = 'P';
+    SELECT p.name
+    FROM products p
+    INNER JOIN providers pr ON p.id_providers = pr.id
+    WHERE p.amount >= 10 AND p.amount <= 20 AND LEFT(pr.name, 1) = 'P';
