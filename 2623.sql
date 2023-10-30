a2573e23e48120d6a608720004b7d0a489de853d
Pedro Mattos <pedro.hmattos19@gmail.com>
2023-10-29 23:06:29 -0300

    Update Oct 29th, 23

--- 2623.sql
+++ 2623.sql
@@ -1,4 +1,6 @@
-SELECT c.name
-FROM customers c
-JOIN legal_person l ON c.id = l.id_customers
-WHERE l.cnpj IS NOT NULL;
+SELECT p.name AS product_name, c.name AS category_name
+FROM products p
+JOIN categories c ON p.id_categories = c.id
+WHERE p.amount > 100
+AND p.id_categories IN (1, 2, 3, 6, 9)
+ORDER BY c.id;
