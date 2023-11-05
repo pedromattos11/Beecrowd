e9f64c1f2ddb5791a9da8bde20c75c030a71535c
Pedro Mattos <pedro.hmattos19@gmail.com>
2023-11-05 10:23:01 -0300

    Update Nov 05th, 23

---
+++ 1009.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nome;
+        double salarioFixo, totalVendas, comissao, salarioTotal;
+
+        nome = Console.ReadLine();
+        salarioFixo = double.Parse(Console.ReadLine());
+        totalVendas = double.Parse(Console.ReadLine());
+
+        comissao = totalVendas * 0.15;
+        salarioTotal = salarioFixo + comissao;
+
+        Console.WriteLine("TOTAL = R$ " + salarioTotal.ToString("F2"));
+    }
+}
