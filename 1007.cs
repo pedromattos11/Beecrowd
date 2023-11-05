61bba41546e15e1b73d75f7a2dde0b88033acf6c
Pedro Mattos <pedro.hmattos19@gmail.com>
2023-11-05 10:14:14 -0300

    Update Nov 05th, 23

---
+++ 1007.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int A, B, C, D;
+
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+        C = int.Parse(Console.ReadLine());
+        D = int.Parse(Console.ReadLine());
+
+        int diferenca = (A * B) - (C * D);
+
+        Console.WriteLine("DIFERENCA = " + diferenca);
+    }
+}
