4054ad66ad8ff641c4725b11a7ffcdc6b209f040
Pedro Mattos <pedro.hmattos19@gmail.com>
2023-11-07 23:18:15 -0300

    Update Nov 07th, 23

--- 1005.py
+++ 1005.py
@@ -1,10 +1,4 @@
-nota1 = float(input("Digite a primeira nota: "))
-nota2 = float(input("Digite a segunda nota: "))
-
-peso1 = 3.5
-peso2 = 7.5
-
-
-media_ponderada = (nota1 * peso1 + nota2 * peso2) / (peso1 + peso2)
-
-print(f"A média ponderada é: {media_ponderada:.5f}")
+a = float(input())
+b = float(input())
+MEDIA = (a*3.5 + b*7.5)/11
+print('MEDIA = {:.5f}'.format(MEDIA))
