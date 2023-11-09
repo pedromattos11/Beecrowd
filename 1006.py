5f2bbe83d613691a631aed9c09c968b875e28a22
Pedro Mattos <pedro.hmattos19@gmail.com>
2023-11-09 20:19:21 -0300

    Update Nov 9th, 23

---
+++ 1006.py
@@ -0,0 +1,10 @@
+# Entrada de dados
+a = float(input())
+b = float(input())
+c = float(input())
+
+# Cálculo da média ponderada
+media_ponderada = (a * 2 + b * 3 + c * 5) / 10
+
+# Saída do resultado usando format // versões diferentes
+print('MEDIA = {:.1f}'.format(media_ponderada))
