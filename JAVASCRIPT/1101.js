1e4ec7680e206795451b5675b58ea6988a06c583
Pedro Mattos <70417778+pedromattos11@users.noreply.github.com>
2023-11-30 20:41:26 -0300

    Update Nov 30th, 23

--- JAVASCRIPT/1101.js
+++ JAVASCRIPT/1101.js
@@ -10,14 +10,7 @@ let m, n;
 rl.on('line', (line) => {
   [m, n] = line.split(' ').map(Number);
 
-  if (m <= 0 || n <= 0) {
-    return;
-  }
-
-  const arr = Array(n - m + 1);
-  if (arr.length === 0) {
-    arr.push(m);
-  }
+  const arr = Array(Math.max(n - m + 1, 1));
 
   console.log(`Sequência de ${m} até ${n}: ${arr.join(', ')}`);
   console.log(`Soma: ${(m + n) * Math.floor((n - m) / 2)}`);
